58ef8a1ecaa5dbe871ff554294cd02de42bb2968
Le Tuan Tai <tailt@younetgroup.com>
2020-07-17 16:22:56 +0700

    Change file name

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { ShareHandler } from './src/ShareHandler'
 import { AsyncStorageHelper } from './src/AsyncStorageHelper'
 import { KeepAwake } from './src/KeepAwake'
 import { DisplayMetrics } from './src/DisplayMetrics'
-import { NativeUtils } from './src/Utils'
+import { NativeUtils } from './src/NativeUtils'
 
 export { ImageCompresser, ShareHandler, AsyncStorageHelper, KeepAwake, DisplayMetrics, NativeUtils }
